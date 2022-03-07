aa50562e9cb2beada65699d721a8f3bb6d59d79e
vĩnh nguyên <nguyena4pct2017@gmail.com>
2022-03-07 12:04:16 +0700

    release conmeobietyeu project

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'c u smile';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'i just want to';
+const titleCard = '8/3/2022';
+const contentCard = 'On this special day, celebrate life. Take a break from your busy schedule. Let your hair down, have fun and do what your heart says. Coz today is your day. Have a great Women’s Day!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'https://scontent.fsgn5-5.fna.fbcdn.net/v/t39.30808-6/273513393_1561530177549650_6609164234448354483_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=174925&_nc_ohc=-m7eAQmWFjgAX9I16pW&_nc_oc=AQn8IgxIIcQ5WmFNTEX0A3QYrwP1tAPTcwza3TEz7rAh_5SAGH0odikKbuN0eGNscA0&_nc_ht=scontent.fsgn5-5.fna&oh=00_AT-RGv7Rqzgx9AvQBUT1RlZXfJ8aAI1PTB6TfxmYOXOKsw&oe=622B61F7';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
